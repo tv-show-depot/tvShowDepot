39e0686ac5df9787d6b27607be3a7e4f690ba59a
Anh Dang <atd30392@gmail.com>
2020-06-10 22:06:06 -0400

    added function to display matched show on the page

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.fetchShow = (query) => {
         method: "GET",
         dataType: "json",
         data: {
-            dataType: "json",
+            format: "json",
             q: query
         }
         
     }).then((result) => {
-        console.log(result);
+        app.displayMatchedShow(result)
     }).fail((error) => {
         console.log("ERROR");
     })
@@ -26,15 +26,28 @@ app.showToLookUp = () => {
         e.preventDefault();
         const userInput = $('input').val();
         app.fetchShow(userInput);
-        
-})
+    })
+}
+
+// Function to display returned result on the page
+app.displayMatchedShow = (returnedShow) => {
+    $('.results').empty();
+
+    const title = $('<h2>').text(returnedShow.name)
+    const image = $('<img>').attr('src',returnedShow.image.original);
+    const language = $('<p>').text(`Language: ${returnedShow.language}`);
+    const genres = $('<p>').text(`Genres: ${returnedShow.genres}`);
+    const rating = $('<p>').text(`Rating: ${returnedShow.rating.average}`);
+    const summary = `Summary: ${returnedShow.summary}`;
+    const showContainer = $('<div>').append(title, image, language, genres, rating, summary);
+
+    $('.results').append(showContainer);
 }
     
 
 
 // A button that allows user to start the process, which will navigate them to a search section.
 // Listen to the button click event to scroll to Search section.
-// Prevent default
 // A search bar and input field so user can type in the show's name
 // Create an eventlistener on submit button, prevent default behavior
 // Get value from user and store in a variable
